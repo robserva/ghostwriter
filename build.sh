1049850df907ad8a1beca5e4be9b20276265401d
Brock Wilcox <awwaiid@thelackthereof.org>
2024-11-01 20:10:34 -0400

    Make it easier to specify ssh targert during build

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-cross build --release --target=armv7-unknown-linux-gnueabihf && scp target/armv7-unknown-linux-gnueabihf/release/ghostwriter remarkable:
+# Set the hostname for the remarkable based on $1 and fall back to `remarkable`
+remarkable="${1:-remarkable}"
+
+cross build --release --target=armv7-unknown-linux-gnueabihf && scp target/armv7-unknown-linux-gnueabihf/release/ghostwriter root@$remarkable:
+
